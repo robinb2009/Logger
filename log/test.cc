2407ae30a8c9d9f122b2b01d49ac2f342cf6b334
Robin Babu Padamadan <robinb2009@gmail.com>
2025-02-24 00:04:32 +0530

    Test: For implementation hints

--- log/test.cc
+++ log/test.cc
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+// We meed a class that pretends to be a stream operator and stores the data for a single logline. (Like Logger)
+// Abuse the fact that destructor will get called after all the '<<' operators are called. 
+// It's the only way to detect end of logline.
+// We don't need to use macros? --> Necessary only for filename and linenumber.
+
+class Logger {
+public:
+    Logger(string name) : name_(move(name)) {
+        cout << "Logger " << name_ << endl;
+    }
+
+private:
+    string name_;
+};
+
+Logger& operator<<(Logger& lg, const int& x) {
+    cout << x << endl;
+    return lg;
+}
+
+class Dummy {
+public:
+    Dummy() { cout << "Dummy created" << endl; }
+    ~Dummy() { cout << "Dummy destroyed" << endl; }
+};
 
 int main() {
-    std::cout << "Hello world";
+    Dummy();
+    auto log = Logger("Log1234");
+    log << 5 << 4 << 3;
+    return 0;
 }
